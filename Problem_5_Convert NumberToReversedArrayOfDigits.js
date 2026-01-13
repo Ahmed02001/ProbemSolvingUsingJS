8ddf9a7967af1ea1ebe1ecdc0d74faba34da8f31
Ahmed02001 <ahmedhamdyy77@gmail.com>
2026-01-13 14:16:51 +0200

    problem number 5 for counts the number of sheep present in the array

--- Problem_5_Convert NumberToReversedArrayOfDigits.js
+++ Problem_5_Convert NumberToReversedArrayOfDigits.js
@@ -1,5 +1,5 @@
 // Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
-
+// link on coodewars    https://www.codewars.com/kata/5583090cbe83f4fd8c000051/train/javascript
 //way 1
 
 function digitize(n){
