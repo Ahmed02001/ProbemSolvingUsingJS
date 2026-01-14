77842fb273dd5f1c42333e4817534976e9910998
Ahmed02001 <ahmedhamdyy77@gmail.com>
2026-01-14 12:38:48 +0200

    function that does four basic mathematical operations

---
+++ Problem_13_BasicMathematicalOperations.js
@@ -0,0 +1,50 @@
+/**
+ * 
+ * Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7
+
+
+link    https://www.codewars.com/kata/57356c55867b9b7a60000bd7
+ */
+
+//way 1
+function basicOp(operation, value1, value2){
+  //Code
+
+  switch(operation){
+    case '+':
+      return value1 + value2;
+    case '*':
+      return value1 * value2;
+    case '-':
+      return value1 - value2;
+    case '/':
+      return value1 / value2;
+  }
+  return 0;
+}
+
+console.log(basicOp('+', 10, 5));
+console.log(basicOp('-', 10, 5));
+console.log(basicOp('*', 10, 5));
+console.log(basicOp('', 10, 5));
+
+
+// way 2
+
+function basicOp(operation, value1, value2){
+   return eval(value1 + operation + value2);
+}
+
+console.log(basicOp('+', 10, 5));
+console.log(basicOp('-', 10, 5));
+console.log(basicOp('*', 10, 5));
+console.log(basicOp('', 10, 5));
